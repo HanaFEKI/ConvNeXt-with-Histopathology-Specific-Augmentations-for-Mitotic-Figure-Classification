28884a776de0234537de578c81496915e53ca001
Hana Feki <hana.feki@ensta.fr>
2025-08-29 15:42:45 +0200

    Update convnext.py

--- models/convnext.py
+++ models/convnext.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import timm
 
 class ConvNextV2Classifier(nn.Module):
-    def __init__(self, model_name: str = "convnext_base", 
-                 weights: str = "DEFAULT", 
+    def __init__(self, model_name: str = "convnext_base",
+                 weights: str = "DEFAULT",
                  num_classes: int = 1):
         super().__init__()
         self.model = timm.create_model(model_name, pretrained=True, num_classes=num_classes)
@@ -12,11 +12,12 @@ class ConvNextV2Classifier(nn.Module):
 
     def forward(self, x):
         logits = self.model(x)
-        if self.num_classes == 1:  # classification binaire
+        if self.num_classes == 1:  # Binary classification
             logits = logits.squeeze(1)
             y_prob = torch.sigmoid(logits)
             y_hat = (y_prob > 0.5).float()
-        else:  # multi-classes
+        else:  # Multi-class
             y_prob = torch.softmax(logits, dim=1)
             y_hat = torch.argmax(y_prob, dim=1)
         return logits, y_prob, y_hat
+
