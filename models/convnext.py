e95a9fdbe47b23a98d253f6417a03a337f8b792c
Hana Feki <hana.feki@ensta.fr>
2025-08-29 15:42:19 +0200

    Create convnext.py

---
+++ models/convnext.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import timm
+
+class ConvNextV2Classifier(nn.Module):
+    def __init__(self, model_name: str = "convnext_base", 
+                 weights: str = "DEFAULT", 
+                 num_classes: int = 1):
+        super().__init__()
+        self.model = timm.create_model(model_name, pretrained=True, num_classes=num_classes)
+        self.num_classes = num_classes
+
+    def forward(self, x):
+        logits = self.model(x)
+        if self.num_classes == 1:  # classification binaire
+            logits = logits.squeeze(1)
+            y_prob = torch.sigmoid(logits)
+            y_hat = (y_prob > 0.5).float()
+        else:  # multi-classes
+            y_prob = torch.softmax(logits, dim=1)
+            y_hat = torch.argmax(y_prob, dim=1)
+        return logits, y_prob, y_hat
